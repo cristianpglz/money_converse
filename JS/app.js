7d2260e8e5643ec771cd1977c989bf6b9826fcfb
cristian <cristianpglez0@gmail.com>
2023-12-26 12:11:24 +0100

    JS update 2

--- JS/app.js
+++ JS/app.js
@@ -3,28 +3,15 @@
 */
 
 //Initialization of var, objects, DOM
-const contentinput=parseFloat(document.getElementById("content").value);
-const currencyinput=document.getElementById("currency");
-console.log(currencyinput);
-var content_resultinput=parseFloat(document.getElementById("currency_result").value);
-const currency_resultinput=document.getElementById("currency_result");
+const currencyinput = document.getElementById("currency").value;
+const currency_resultinput=document.getElementById("currency_result").value;
+const contentinput= parseInt(document.getElementById("content").value)
 const error=document.getElementById("error");
-
+var euro = 1;
+var dolar = 1.10;
+var converting = 0;
 //Event functions
 
-
-function convert(event){
-    if(currencyinput.value === currency_resultinput.value){
-        error.innerText="Por favor , Utilice una moneda diferente"
-        return false;
-    }
-    else{
-        switch(currency_resultinput.value){
-            case "€":{
-            currency_resultinput.value = contentinput.value * 0.60;
-            return currency_resultinput.value;
-        }
-    }
-    }
+function convert(){
+    console.log(contentinput);
 }
-console.log(convert());
